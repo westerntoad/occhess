70d967a045979fcc8f2badba40676790a75c6615
westerntoad <91150531+westerntoad@users.noreply.github.com>
2022-05-10 18:38:52 -0700

    added puzzle of the day & more responsive

--- assets/js/backgroundScroll.js
+++ assets/js/backgroundScroll.js
@@ -7,6 +7,13 @@ var mtnDiv;
 window.addEventListener('load', (event) => {
     mtnDiv = document.querySelector("#mtn-div");
     console.log('page is fully loaded');
+    let y = (window.scrollY || window.pageYOffset) / (mtnDiv.offsetHeight * 0.9)
+    if(y > 1) y = 1
+
+    var r = mtnColor[0] * (1 - y) + bgColor[0] * y
+    var g = mtnColor[1] * (1 - y) + bgColor[1] * y
+    var b = mtnColor[2] * (1 - y) + bgColor[2] * y
+    root.style.setProperty('--mountain', `rgb(${r}, ${g}, ${b})`)
   });
 
 
