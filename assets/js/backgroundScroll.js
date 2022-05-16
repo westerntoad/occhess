475cf52726abbee94dcd529c3a9db9661f3754db
westerntoad <91150531+westerntoad@users.noreply.github.com>
2022-05-15 21:02:20 -0700

    sorted js and added schedule

--- assets/js/backgroundScroll.js
+++ assets/js/backgroundScroll.js
@@ -1,12 +1,26 @@
 // GLOBAL VARIABLES
-var root = document.querySelector(':root')
-var rootStyles = getComputedStyle(root)
-
+var root;
+var rootStyles;
 var mtnDiv;
+var mtnColor;
+var bgColor;
 
 window.addEventListener('load', (event) => {
+    root = document.querySelector(':root');
+    rootStyles = getComputedStyle(root);
     mtnDiv = document.querySelector("#mtn-div");
+    mtnColor = rootStyles.getPropertyValue('--mountain')
+      .toString().replace(/[^\d,]/g, '').split(','); // parsing rgb to array
+    bgColor = rootStyles.getPropertyValue('--background')
+      .toString().replace(/[^\d,]/g, '').split(','); // parsing rgb to array
+
+    updateBackground();
     console.log('page is fully loaded');
+  })
+
+
+// FUNCTIONS
+function updateBackground() {
     let y = (window.scrollY || window.pageYOffset) / (mtnDiv.offsetHeight * 0.9)
     if(y > 1) y = 1
 
@@ -14,29 +28,10 @@ window.addEventListener('load', (event) => {
     var g = mtnColor[1] * (1 - y) + bgColor[1] * y
     var b = mtnColor[2] * (1 - y) + bgColor[2] * y
     root.style.setProperty('--mountain', `rgb(${r}, ${g}, ${b})`)
-  });
+}
 
 
-
-
-
-
-
-
-
-
-// BACKGROUND SCROLL
-var mtnColor = rootStyles.getPropertyValue('--mountain')
-    .toString().replace(/[^\d,]/g, '').split(','); // parsing rgb to array
-var bgColor = rootStyles.getPropertyValue('--background')
-    .toString().replace(/[^\d,]/g, '').split(','); // parsing rgb to array
-
+// EVENT LISTENERS
 window.addEventListener('scroll', () => {
-    let y = (window.scrollY || window.pageYOffset) / (mtnDiv.offsetHeight * 0.9)
-    if(y > 1) y = 1
-
-    var r = mtnColor[0] * (1 - y) + bgColor[0] * y
-    var g = mtnColor[1] * (1 - y) + bgColor[1] * y
-    var b = mtnColor[2] * (1 - y) + bgColor[2] * y
-    root.style.setProperty('--mountain', `rgb(${r}, ${g}, ${b})`)
-})
+    updateBackground();
+  })
